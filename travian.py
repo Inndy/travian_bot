3eddc4a35dec58161d843984a41c69d283574c06
Inndy <Inndy.mail@gmail.com>
2014-12-07 16:44:32 +0800

    Read status

--- travian.py
+++ travian.py
@@ -6,6 +6,8 @@ class TravianPageStatus(object):
     SIG_LOGIN_FAILED = '''<input class="text" type="password" name="pw" value=""'''
 
 class TravianClient(object):
+    RESOURCES_NAME = "木材 磚塊 鋼鐵 穀物".split()
+
     def __init__(self, config):
         """ initialize instance
 
@@ -27,7 +29,53 @@ class TravianClient(object):
             's1.y': 0
         }
         response = self.session.post(self.config.url('login.php'), data = data)
-        return not TravianPageStatus.SIG_LOGIN_FAILED in response.text
+        self.last_response = response
+        if TravianPageStatus.SIG_LOGIN_FAILED not in response.text:
+            self.last_info = response
+            return True
+        else:
+            return False
+
+    def request_dorf1(self, response = None):
+        if not response:
+            response = self.session.get(self.config.url('dorf1.php'))
+        text = response.text.replace('</body>', '').replace('</html>', '')
+        return bs4.BeautifulSoup(text)
+
+    def info(self, response = None):
+        if not response:
+            response = self.session.get(self.config.url('dorf1.php'))
+        model = self.request_dorf1(response)
+
+        # Parse tiemr data
+        self.timers = []
+        for i in range(1, 10):
+            timer = model.find('span', attrs = { 'id': 'timer' + str(i) })
+            if timer:
+                self.timers.append(timer.text)
+            else:
+                break
+
+        # Parse resources data
+        self.resources = []
+        for i in range(1, 5):
+            res = model.find('td', attrs = { 'id': 'l' + str(i) },
+                             recursive = True)
+            self.resources.append([ int(n) for n in res.text.split('/') ])
+
+        self.last_info = response
+        return response
+
+    def dump_status(self):
+        result = []
+        for i, (m, n) in enumerate(self.resources):
+            result.append("%s: %d / %d" %
+                          (TravianClient.RESOURCES_NAME[i], m, n))
+        result += self.timers
+        print("\n".join(result))
+
+
+
 
 class TravianConfig(object):
     def __init__(self, base_url, username, password):
@@ -68,6 +116,8 @@ def main():
     client = TravianClient(config)
     if not client.login():
         return 'Login failed'
+    client.info()
+    client.dump_status()
 
     return 0
 
